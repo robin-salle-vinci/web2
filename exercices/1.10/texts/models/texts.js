0e0dab5383f7ef1aacb33fbb6b3de4fbb34137f1
Sallé <robin.salle@student.vinci.be>
2023-10-10 12:38:48 +0200

    1:10 update

--- exercices/1.10/texts/models/texts.js
+++ exercices/1.10/texts/models/texts.js
@@ -3,22 +3,22 @@ const { parse} = require('../utils/json');
 
 const jsonDbPath = path.join(__dirname, '/../data/texts.json');
 
-// eslint-disable-next-line import/no-extraneous-dependencies, import/order
-const { v4: uuidv4 } = require('uuid');
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('uuid');
 
 const TEXTS = [
   {
-    id: uuidv4(),
+    id: "94ad866a-5af7-49aa-9b6e-a618cddf9de2",
     content: "C'est le contenu textuel",
     level: 'medium',
   },
   {
-    id: uuidv4(),
+    id:  "5967a786-b871-406e-a9af-8b601aa8a4d5",
     content: "C'est le contenu textuel version 2",
     level: 'hard',
   },
   {
-    id: uuidv4(),
+    id: "97adac22-8d80-4abc-a339-16ca9771cffb" ,
     content: "C'est le contenu textuel version 3",
     level: 'easy',
   },
@@ -37,7 +37,7 @@ function readAllTexts(filter) {
 
 function readOneText(id) {
     const texts = parse(jsonDbPath, TEXTS);
-    const indexOfTextFound = texts.find(t => t.id === id);
+    const indexOfTextFound = texts.findIndex(t => t.id === id);
 
     if(indexOfTextFound < 0) {
         return undefined;
