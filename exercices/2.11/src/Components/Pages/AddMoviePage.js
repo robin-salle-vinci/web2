d580497653cfdc9408733fd41dac3870c4436884
Sallé Robin <robin.salle@student.vinci.be>
2023-11-07 11:34:57 +0100

    2.11.1 : router & static pages

---
+++ exercices/2.11/src/Components/Pages/AddMoviePage.js
@@ -0,0 +1,8 @@
+const AddMoviePage = () => {
+    const main = document.querySelector('main');
+    main.innerHTML = `<div class="text-center">
+    <h3>page d'ajout film</h3>
+    `;
+}
+
+export default AddMoviePage;
