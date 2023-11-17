7eadd621c0b74bd003c503a564f53d38959cc89c
Sallé Robin <robin.salle@student.vinci.be>
2023-11-17 15:02:14 +0100

    2.11.2 : add films.

--- exercices/2.11/src/Components/Pages/AddMoviePage.js
+++ exercices/2.11/src/Components/Pages/AddMoviePage.js
@@ -1,8 +1,11 @@
 import { clearPage } from "../../utils/render";
 
+const MOVIES = [];
+
 const AddMoviePage = () => {
   clearPage();
   renderAddMoviePage();
+  renderInputForm();
 }
 
 function renderAddMoviePage () {
@@ -28,10 +31,33 @@ function renderAddMoviePage () {
         <label class="form-label" for="descriptionMovie">Description :</label>
         <input type="link" class="form-control" id="descriptionMovie" required>
       </div>
-    <button type="submit" class="btn btn-primary">Soumettre</button>
+    <input type="submit" class="btn btn-primary" value="Soumettre"></input>
     </form>
     </div>
     `;
+}
+
+function renderInputForm () {
+  const form = document.querySelector('form');
+  form.addEventListener('submit',addMovie);
+}
+
+function addMovie (e) {
+  e.preventDefault(); // pour empêcher que la page se reload
+  const title = document.getElementById('titleMovie').value;
+  const duration = document.getElementById('durationMovie').value;
+  const budget = document.getElementById('budgetMovie').value;
+  const description = document.getElementById('descriptionMovie').value;
+
+const movie = {
+  title,
+  duration,
+  budget,
+  description
+}
+MOVIES.push(movie);
+// eslint-disable-next-line no-console
+console.log(movie);
 }
 
 export default AddMoviePage;
