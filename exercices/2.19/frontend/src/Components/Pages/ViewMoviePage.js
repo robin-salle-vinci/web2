4f224c22eba5e7d7351d0ec7bd4cd8ba6d3b14dc
Sallé Robin <robin.salle@student.vinci.be>
2023-11-25 11:53:05 +0100

    2.19.4 : spa update operation.

--- exercices/2.19/frontend/src/Components/Pages/ViewMoviePage.js
+++ exercices/2.19/frontend/src/Components/Pages/ViewMoviePage.js
@@ -1,9 +1,14 @@
 import { clearPage } from '../../utils/render';
-import {getAllMovies} from '../../models/movie';
+import {getAllMovies, deleteOneMovie, UpdateOneMovie} from '../../models/movie';
 
-const ViewMoviePage = () => {
+
+
+
+const ViewMoviePage = async () => {
     clearPage();
-    renderViewMoviePage();
+    await renderViewMoviePage();
+    clickOnButtonDelete();
+    clickOnButtonSave();
 }
 
 async function renderViewMoviePage() {
@@ -28,23 +33,68 @@ async function getMoviesTable() {
     <thead>
   <tr>
     <th scope="col">Title</th>
+    <th scope="col">Link</th>
     <th scope="col">Duration (min)</th>
-    <th scope="col">Budget (million)</th>    
+    <th scope="col">Budget (million)</th>
+    <th scope="col">Operations</th>  
   </tr>
 </thead>
 <tbody>
     `;
     movies.forEach((element) => {
         movieTable += `<tr>
-        <td><a href="${element.link}">${element.title}</a></td>
-        <td>${element.duration}</td>
-        <td>${element.budget}</td>
+        <td contenteditable="true">${element.title}</td>
+        <td contenteditable="true">${element.link}</td>
+        <td contenteditable="true">${element.duration}</td>
+        <td contenteditable="true">${element.budget}</td>
+        <td><button class="btn btn-primary delete" data-id="${element.id}">Delete</button>
+        <button class="btn btn-primary update" data-id="${element.id}">Save</button>
+        </td>
         </tr>
         `;
     });
     movieTable += `</tbody>
     </table>`;
     return movieTable;
+}
+
+function clickOnButtonDelete() {
+    const buttonsDeleted = document.querySelectorAll('.delete');
+    buttonsDeleted.forEach(button => {
+        button.addEventListener('click', async () => {
+            await deleteOneMovie(button.getAttribute('data-id'));
+            ViewMoviePage();
+        });
+    });
+};
+
+function clickOnButtonSave() {
+    const buttonsSaved = document.querySelectorAll('.update');
+    buttonsSaved.forEach(button => {
+        button.addEventListener('click', async (e) => {
+            const id = button.getAttribute('data-id');
+            const title = e.target.parentElement.parentElement.children[0].innerText;
+            const link = e.target.parentElement.parentElement.children[1].innerText;
+            const duration = parseInt(e.target.parentElement.parentElement.children[2].innerText, 10);
+            const budget = parseInt(e.target.parentElement.parentElement.children[3].innerText, 10);
+
+            const options = {
+                method: 'PATCH',
+                body:JSON.stringify({
+                  title,
+                  link,
+                  duration,
+                  budget,
+                }),
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              }
+
+            await UpdateOneMovie(options, id);
+            ViewMoviePage();
+        });
+    });
 }
 
 export default ViewMoviePage;
--- exercices/2.19/frontend/src/models/movie.js
+++ exercices/2.19/frontend/src/models/movie.js
@@ -1,3 +1,5 @@
+
+
 async function getAllMovies() {
   try {
     const response = await fetch('/api/films');
@@ -15,4 +17,18 @@ async function addOneMovie(options) {
     if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
 };
 
-export { getAllMovies, addOneMovie };
+async function deleteOneMovie(id) {
+  const options = {
+    method: 'DELETE',
+  };
+    const response = await fetch(`/api/films/${id}`, options); // fetch return a promise => we wait for the response
+    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+}
+
+async function UpdateOneMovie(options, id) {
+  const response = await fetch(`/api/films/${id}`, options); // fetch return a promise => we wait for the response
+  if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+};
+
+
+export { getAllMovies, addOneMovie, deleteOneMovie, UpdateOneMovie };
